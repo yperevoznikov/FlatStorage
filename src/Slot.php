d8adb96575ab88b6374a5d8898b6223f16de379c
Yuriy Perevoznikov <yuriless@gmail.com>
2015-08-24 12:42:24 +0300

    Implemented Slot class interface and tests for it

--- src/Slot.php
+++ src/Slot.php
@@ -4,6 +4,35 @@ namespace YPStorageEngine;
 
 class Slot {
 
+    private $data = array();
 
+    public function __construct($fields=array()){
+        foreach ($fields as $name => $value) {
+            $this->data[$name] = $value;
+        }
+    }
+
+    public function __get($name){
+        return isset($this->data[$name]) ? $this->data[$name] : null;
+    }
+
+    public function __set($name, $value){
+        $this->data[$name] = $value;
+    }
+
+    public function matchCriteria($criteria){
+        foreach ($criteria as $name => $value) {
+            if (!isset($this->data[$name]) || $this->data[$name] != $value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function update($fields) {
+        foreach ($fields as $name => $value) {
+            $this->__set($name, $value);
+        }
+    }
 
 }
---
+++ tests/SlotTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace YPStorageEngine;
+
+class SlotTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var \YPStorageEngine\Slot
+     */
+    private $slot;
+
+    /**
+     *  Prepare each test with pure fixture
+     */
+    public function setUp() {
+        $this->slot = new Slot();
+    }
+
+    /**
+     * @dataProvider matchCriteriaProvider
+     */
+    public function testMatchCriteria($inputFields, $criteria, $expected){
+        $slot = new Slot($inputFields);
+        $res = $slot->matchCriteria($criteria);
+        $this->assertEquals($expected, $res);
+    }
+
+    public function matchCriteriaProvider() {
+        return array(
+            array(array('f1'=>'v1'), array('f1'=>'v1'), true),
+            array(array('f1'=>'v1'), array('f1'=>'v2'), false),
+            array(array('f1'=>'v1'), array('m'=>'n', 'f1'=>'v1'), false),
+            array(array('m'=>'n', 'f1'=>'v1'), array('f1'=>'v1'), true),
+        );
+    }
+
+    public function testUpdate(){
+        $this->slot->update(array('f1' => 'v1'));
+        $this->assertEquals('v1', $this->slot->f1);
+    }
+
+}
